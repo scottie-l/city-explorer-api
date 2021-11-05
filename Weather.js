00253a144c0877e8dd327d177d2632de3c02db1a
Scott Lease <scottie_lease@hotmail.com>
2021-11-05 14:51:57 -0700

    added movieDB backend server

--- Weather.js
+++ Weather.js
@@ -1,9 +1,12 @@
-import axios from "axios";
-import { response } from "express";
+// import axios from "axios";
+// import { response } from "express";
 
-export default class Weather extends Component {
+// export default class Weather extends Component {
 
-    getWeather = async(city) => {
-        const url = `https://api.weatherbit.io/v2.0/subscription/usage?key=${process.env.REACT_APP_WEATHER_KEY}&q=${city}&format=json`;
-    }
-}
+//     getWeather = async(city) => {
+//         const url = `https://api.weatherbit.io/v2.0/subscription/usage?key=${process.env.REACT_APP_WEATHER_KEY}&q=${city}&format=json`;
+//     }
+// }
+
+
+// Save for modularity
--- server.js
+++ server.js
@@ -3,8 +3,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const { default: axios } = require('axios');
+const { query } = require('express');
+const app = express();
 
 app.use(cors());
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 3001;
 
 app.get('/hello', (req, res) => res.status(200).send('Hello World'));
 app.get('/weather', handleGetWeatherList); 
+app.get('/movies', handleGetMovie)
 
 async function handleGetWeatherList(req, res) {
   try {
@@ -31,6 +33,29 @@ class Forecast {
   constructor(obj) {
     this.description = obj.weather.description;
     this.data = obj.datetime;
+  }
+}
+
+async function handleGetMovie(req, res) {
+  try {
+    const {city} = req.query;
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIEDB_KEY}&query=${city}&language=en-US&page=1&include_adult=true`;
+    console.log(url);
+    const results = await axios.get(url);
+    console.log(results.data.results);
+    const movieData = results.data.results.map(movies => new Movies(movies));
+    console.log(movieData);
+    res.status(200).send(movieData);
+  } catch (event) {
+    res.status(500).send('Server Error 500')
+  }
+}
+
+class Movies {
+  constructor(obj) {
+    this.title = obj.title;
+    this.overview = obj.overview;
+    this.image = `https://image.tmdb.org/t/p/w500${obj.poster_path}`;
   }
 }
 
